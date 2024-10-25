de38908cebdaa8142b279b7b04aca0cf6e47c2a9
venkatneelapu <neelapu.venkatanaidu@gmail.com>
2024-10-25 23:47:13 +0530

    first commit

--- src/sections/education/Education.jsx
+++ src/sections/education/Education.jsx
@@ -5,6 +5,36 @@ const Education = () => {
   return (
     <section id='education'>
       <h2>Education</h2>
+      <div className="education-container">
+        <div className="education-card">
+          <h3>BTech in Computer Science</h3>
+          <p>Lovely Professional University</p>
+          <p>2020-2024</p>
+
+          <h3>Intermediate</h3>
+          <p>Lovely Professional University</p>
+          <p>2018-2020</p>
+
+          <h3>SSC</h3>
+          <p>Lovely Professional University</p>
+          <p>2016-2018</p>
+        </div>
+
+        <div className="edu-card">
+          <h3>BTech in Computer Science</h3>
+          <p>Lovely Professional University</p>
+          <p>2020-2024</p>
+
+          <h3>Intermediate</h3>
+          <p>Lovely Professional University</p>
+          <p>2018-2020</p>
+
+          <h3>SSC</h3>
+          <p>Lovely Professional University</p>
+          <p>2016-2018</p>
+        </div>
+        
+      </div>
     </section>
   )
 }
