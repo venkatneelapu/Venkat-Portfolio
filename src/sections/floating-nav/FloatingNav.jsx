9ea6dbb2ce3f7986d13f4da7b5aea4730bf31074
venkatneelapu <neelapu.venkatanaidu@gmail.com>
2024-10-26 00:03:51 +0530

    removed a code

--- src/sections/floating-nav/FloatingNav.jsx
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -3,7 +3,6 @@ import './floating-nav.css';
 import { AiOutlineHome } from "react-icons/ai";
 import { FaRegUserCircle } from "react-icons/fa";
 import { FaBook } from "react-icons/fa";
-import { RiServiceLine } from "react-icons/ri";
 import { GrContact } from "react-icons/gr";
 import { IoSchoolSharp } from "react-icons/io5";
 import {useState} from 'react';
@@ -11,7 +10,7 @@ import {useState} from 'react';
 const FloatingNav = () => {
   const [activeNav, setActiveNav] = useState('#');
   return (
-    <section id='floating__nav' className='floatingnav'>
+    <section id='floating__nav' className='floating_nav'>
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaRegUserCircle /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaBook /></a>
