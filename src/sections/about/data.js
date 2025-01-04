16c30e922141a334a977537d41170ce6b9f48665
venkatneelapu <neelapu.venkatanaidu@gmail.com>
2025-01-04 13:20:13 +0530

    about us change

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -4,8 +4,8 @@ import { BiHappyHeartEyes } from "react-icons/bi";
 
 const data = [
     {id:1,icon:<FaAward />,title:'Experience',desc:'Studying'},
-    {id:2,icon:<TbBooks />,title:'Projects',desc:'3+ compleated'},
-    {id:3,icon:<BiHappyHeartEyes />,title:'Happy clients',desc:'3+ happy clients'},
+    {id:2,icon:<TbBooks />,title:'Projects',desc:'3+ completed'},
+    {id:3,icon:<BiHappyHeartEyes />,title:'Happy clients',desc:'1+ happy clients'},
 ]
 
 export default data;
