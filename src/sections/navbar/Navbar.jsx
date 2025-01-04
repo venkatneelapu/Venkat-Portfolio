c5ec7f65cbf2f947bcf5acbca5bced448cd78f2c
venkatneelapu <neelapu.venkatanaidu@gmail.com>
2025-01-04 12:28:00 +0530

    theme change

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Logo from '../../assets/IMG-20220627-WA0025.jpg';
-import { MdFormatColorFill } from "react-icons/md";
+import { MdFormatColorFill } from 'react-icons/md';
 import './navbar.css';
 import data from './data';
+import Theme from '../../theme/Theme'; 
 
 const Navbar = () => {
+  const [themePopupVisible, setThemePopupVisible] = useState(false);
+
+  const toggleThemePopup = () => {
+    setThemePopupVisible(!themePopupVisible);
+  };
+
+  const changeTheme = (hue) => {
+    document.documentElement.style.setProperty('--primary-hue', hue);
+  };
+
+  useEffect(() => {
+    const savedColor = localStorage.getItem('selectedColor');
+    if (savedColor) {
+      changeTheme(savedColor); 
+    }
+  }, []);
+
   return (
     <nav>
       <div className="container nav__container">
-        <a href="index.html" className='nav__logo'>
+        <a href="index.html" className="nav__logo">
           <img src={Logo} alt="Logo" />
         </a>
-        <ul className='nav__menu'>
-          {
-          data.map(item => <li key={item.id}><a href={item.Link}>{item.title}</a></li>)
-          }
+        <ul className="nav__menu">
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.Link}>{item.title}</a>
+            </li>
+          ))}
         </ul>
-        <button id='theme__icon'><MdFormatColorFill/></button>
+        <button id="theme__icon" onClick={toggleThemePopup}>
+          <MdFormatColorFill />
+        </button>
       </div>
-    </nav>
-  )
-}
 
-export default Navbar
+      {/* Display the Theme popup if the state is true */}
+      {themePopupVisible && (
+        <Theme onClose={toggleThemePopup} changeTheme={changeTheme} />
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/theme/Theme.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import './Theme.css';
+
+const colorOptions = [
+  270, 180, 120, 60, 0, 240, 300, 200, 50, 320 
+];
+
+const Theme = ({ onClose, changeTheme }) => {
+  useEffect(() => {
+    const savedColor = localStorage.getItem('selectedColor');
+    if (savedColor) {
+      changeTheme(savedColor);
+    }
+  }, [changeTheme]);
+
+  const handleColorSelect = (hue) => {
+    changeTheme(hue);
+    localStorage.setItem('selectedColor', hue); 
+    onClose(); 
+  };
+
+  return (
+    <div className="theme-popup">
+      <div className="theme-popup__content">
+        <h2>Select a Theme Color</h2>
+        <div className="theme-popup__colors">
+          {colorOptions.map((hue) => (
+            <button
+              key={hue}
+              className="color-option"
+              style={{ backgroundColor: `hsl(${hue}, 89%, 41%)` }}
+              onClick={() => handleColorSelect(hue)}
+            />
+          ))}
+        </div>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default Theme;
