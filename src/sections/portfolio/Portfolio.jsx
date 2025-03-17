a1b04e2ea1cbd767b02ab657bf56f022e4a0a23c
venkatneelapu <neelapu.venkatanaidu@gmail.com>
2025-03-17 15:54:48 +0530

    changed projects

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -5,6 +5,10 @@ import { MdLiveTv } from "react-icons/md";
 import Vb from '../../assets/vb.png';
 import Sparkv from '../../assets/sparkv.png';
 import Hunger from '../../assets/hungerbird.png';
+import TODO from '../../assets/todo.png';
+import Med from '../../assets/medicals.png';
+import wik from '../../assets/wik.png';
+import food from '../../assets/food.png';
 
 
 
@@ -14,26 +18,63 @@ const Portfolio = () => {
       <h5>My Resent Projects</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
+
+      <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={Sparkv} alt="" />
+          </div>
+            <h5>SparkV's Roadmaps</h5>
+            <div className="portfolio__item-cta">
+            <a href="https://github.com/venkatneelapu/SparkV_Roadmaps" className='btn' target='_blank'><FaGithub />Github</a>
+            <a href="https://sparkv-roadmaps.netlify.app/home" className='btn primary' target='_blank'><MdLiveTv />Demo</a>
+            </div>
+        </article>
+
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
             <img src={Vb} alt="" />
           </div>
             <h5>VB Computers</h5>
             <div className="portfolio__item-cta">
-            <a href="https://github.com/venkatneelapu" className='btn' target='_blank'><FaGithub />Github</a>
+            <a href="https://github.com/venkatneelapu/VB-computers" className='btn' target='_blank'><FaGithub />Github</a>
             <a href="https://vb-computers.onrender.com/" className='btn primary' target='_blank'><MdLiveTv />Demo</a>
             </div>
         </article>
+        
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={Sparkv} alt="" />
+            <img src={Med} alt="" />
           </div>
-            <h5>SparkV's Roadmaps</h5>
+            <h5>Bhavani-Medicals & General Store</h5>
             <div className="portfolio__item-cta">
-            <a href="https://github.com/venkatneelapu" className='btn' target='_blank'><FaGithub />Github</a>
-            <a href="https://sparkv-roadmaps.netlify.app/" className='btn primary' target='_blank'><MdLiveTv />Demo</a>
+            <a href="https://github.com/venkatneelapu/Bhavani-Medicals" className='btn' target='_blank'><FaGithub />Github</a>
+            {/* <a href="https://vb-computers.onrender.com/" className='btn primary' target='_blank'><MdLiveTv />Demo</a> */}
             </div>
         </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={wik} alt="" />
+          </div>
+            <h5>Wikipedia Search Application</h5>
+            <div className="portfolio__item-cta">
+            {/* <a href="https://github.com/venkatneelapu" className='btn' target='_blank'><FaGithub />Github</a> */}
+            <a href="https://venkatj107.ccbp.tech/" className='btn primary' target='_blank'><MdLiveTv />Demo</a>
+            </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={TODO} alt="" />
+          </div>
+            <h5>Todos Application</h5>
+            <div className="portfolio__item-cta">
+            {/* <a href="https://github.com/venkatneelapu" className='btn' target='_blank'><FaGithub />Github</a> */}
+            <a href="https://venkatj106.ccbp.tech/" className='btn primary' target='_blank'><MdLiveTv />Demo</a>
+            </div>
+        </article>
+        
+
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
             <img src={Hunger} alt="" />
@@ -44,36 +85,19 @@ const Portfolio = () => {
             {/* <a href="https://vb-computers.onrender.com/" className='btn primary' target='_blank'><MdLiveTv />Demo</a> */}
             </div>
         </article>
-        {/* <article className='portfolio__item'>
+
+        <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={Vb} alt="" />
+            <img src={food} alt="" />
           </div>
-            <h5>This is a portfolio item title</h5>
+            <h5>Food Munch</h5>
             <div className="portfolio__item-cta">
-            <a href="https://github.com/venkatneelapu" className='btn' target='_blank'><FaGithub />Github</a>
-            <a href="https://vb-computers.onrender.com/" className='btn primary' target='_blank'><MdLiveTv />Demo</a>
+            {/* <a href="https://github.com/venkatneelapu" className='btn' target='_blank'><FaGithub />Github</a> */}
+            <a href="https://venkatneelapu50.ccbp.tech/" className='btn primary' target='_blank'><MdLiveTv />Demo</a>
             </div>
-        </article> */}
-        {/* <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={Vb} alt="" />
-          </div>
-            <h5>This is a portfolio item title</h5>
-            <div className="portfolio__item-cta">
-            <a href="https://github.com/venkatneelapu" className='btn' target='_blank'><FaGithub />Github</a>
-            <a href="https://vb-computers.onrender.com/" className='btn primary' target='_blank'><MdLiveTv />Demo</a>
-            </div>
-        </article> */}
-        {/* <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={Vb} alt="" />
-          </div>
-            <h5>This is a portfolio item title</h5>
-            <div className="portfolio__item-cta">
-            <a href="https://github.com/venkatneelapu" className='btn' target='_blank'><FaGithub />Github</a>
-            <a href="https://vb-computers.onrender.com/" className='btn primary' target='_blank'><MdLiveTv />Demo</a>
-            </div>
-        </article> */}
+        </article>
+
+        
       </div>
     </section>
   )
