c5a1794be7d5b7a74d0b2482eca3e1be3d727a85
venkatneelapu <neelapu.venkatanaidu@gmail.com>
2025-04-16 10:30:16 +0530

    navbar change

--- src/sections/navbar/data.js
+++ src/sections/navbar/data.js
@@ -3,7 +3,9 @@ const data=[
     {id:2,Link:'#about',title:'About'},
     {id:3,Link:'#experience',title:'Experience'},
     {id:4,Link:'#portfolio',title:'Projects'},
-    {id:5,Link:'#contact',title:'Contact'},
+    {id:5,Link:'#certifications',title:'Certifications'},
+    {id:6,Link:'#contact',title:'Contact'},
+    
 ]
 
 export default data;
